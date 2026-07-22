1ba16e6603d65de8a50b7b27181f85fc408660df
Menko <menko@hornstein.me>
2026-07-22 09:50:51 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'translator-app',
+  webDir: 'dist'
+};
+
+export default config;
